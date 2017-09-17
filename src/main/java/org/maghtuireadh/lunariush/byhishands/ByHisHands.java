273d1d17a87f5e7f26d4f71ca37c3287b30f7e21
Hatter <Hatter@192.168.0.18>
2017-09-17 00:10:30 -0400

    Updati

--- src/main/java/org/maghtuireadh/lunariush/byhishands/ByHisHands.java
+++ src/main/java/org/maghtuireadh/lunariush/byhishands/ByHisHands.java
@@ -29,3 +29,4 @@ public class ByHisHands {
 		proxy.postInit(event);
 	}
 }
+
---
+++ src/main/java/org/maghtuireadh/lunariush/byhishands/handlers/Registry.java
@@ -0,0 +1,26 @@
+package org.maghtuireadh.lunariush.byhishands.handlers;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import static org.maghtuireadh.lunariush.byhishands.init.ItemInit.tutorial_ingot;
+import static org.maghtuireadh.lunariush.byhishands.init.BlockInit.tutorial_ore;
+
+@EventBusSubscriber(modid = "bhh")
+public class Registry{
+	
+    public static Item[] itemArray= {tutorial_ingot};
+    public static Block[] blockArray= {tutorial_ore};
+    
+    
+	@SubscribeEvent
+    public static void RegisterModels(ModelRegistryEvent array) {
+		for(Item i : itemArray) {
+            ModelLoader.setCustomModelResourceLocation(i, 0, new ModelResourceLocation(i.getRegistryName().toString()));
+        }
+    }
+}
